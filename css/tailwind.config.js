f1006d487c4749648cb4c52132c595e39e3f580b
Dottoonfires <dottoleao@gmail.com>
2024-07-19 17:28:36 -0300

    estrutura

---
+++ css/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        lightRed: "hsl(0, 100%, 67%)",
+        orangeYellow: "hsl(39, 100%, 56%)",
+        greenTeal: "hsl(166, 100%, 37%)",
+        cobaltBlue: "hsl(234, 85%, 45%)",
+        lighSlateBlue: "hsl(252, 100%, 67%)",
+        lightRoyal: "hsl(241, 81%, 54%)",
+        violetBlue: "hsla(256, 72%, 46%, 1)",
+        persianBlue: "hsla(241, 72%, 46%, 0)",
+        paleBlue: "hsl(221, 100%, 96%)",
+        lightLavander: "hsl(241, 100%, 89%)",
+        darkGreyBlue: "hsl(224, 30%, 27%)",
+      },
+    },
+  },
+  plugins: [],
+};
